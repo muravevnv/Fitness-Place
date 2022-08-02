818712d8d40ce13171d106b5de9fc7c50aaf5d87
Nickolay Muravev <murvievnick@gmail.com>
2022-08-03 01:49:50 +0300

    верстка проекта ч2

--- Source/scripts/main.js
+++ Source/scripts/main.js
@@ -3,7 +3,7 @@ const swiper = new Swiper('.js-slider', {
     slidesPerView: 1,
     adaptiveHeight: true,
     pagination: {
-      el: '.swiper-pagination',
+      el: '.slider-pagination',
     },
     navigation: {
       nextEl: '.slider-next',
@@ -15,17 +15,40 @@ const sliderBrands = new Swiper('.js-brands-slider', {
     slidesPerView: 'auto',
     adaptiveHeight: true,
     spaceBetween: 70,
-    // navigation: {
-    //   nextEl: '.slider-next',
-    //   prevEl: '.slider-prev',
-    // },
+    navigation: {
+      nextEl: '.brands-next',
+      prevEl: '.brands-prev',
+    },
   });
 const sliderAfisha = new Swiper('.js-afisha-slider ', {
-    slidesPerView: 4,
+    slidesPerView: 1,
     adaptiveHeight: true,
-    spaceBetween: 30,
-    // navigation: {
-    //   nextEl: '.slider-next',
-    //   prevEl: '.slider-prev',
-    // },
+    spaceBetween: 10,
+
+    pagination: {
+      el: '.afisha-pagination',
+    },
+    navigation: {
+      nextEl: '.afisha-navigation-next',
+      prevEl: '.afisha-navigation-prev',
+    },
+    breakpoints: {
+
+      560: {
+        slidesPerView: 2,
+        adaptiveHeight: true,
+        spaceBetween: 20,
+
+      },
+      991: {
+        slidesPerView: 3,
+        adaptiveHeight: true,
+        spaceBetween: 20,
+      },
+      1440: {
+        slidesPerView: 4,
+        adaptiveHeight: true,
+        spaceBetween: 30,
+      },
+    }
   });
--- tmp/main.js
+++ tmp/main.js
@@ -4,7 +4,7 @@ var swiper = new Swiper('.js-slider', {
   slidesPerView: 1,
   adaptiveHeight: true,
   pagination: {
-    el: '.swiper-pagination'
+    el: '.slider-pagination'
   },
   navigation: {
     nextEl: '.slider-next',
@@ -14,18 +14,38 @@ var swiper = new Swiper('.js-slider', {
 var sliderBrands = new Swiper('.js-brands-slider', {
   slidesPerView: 'auto',
   adaptiveHeight: true,
-  spaceBetween: 70 // navigation: {
-  //   nextEl: '.slider-next',
-  //   prevEl: '.slider-prev',
-  // },
-
+  spaceBetween: 70,
+  navigation: {
+    nextEl: '.brands-next',
+    prevEl: '.brands-prev'
+  }
 });
 var sliderAfisha = new Swiper('.js-afisha-slider ', {
-  slidesPerView: 4,
+  slidesPerView: 1,
   adaptiveHeight: true,
-  spaceBetween: 30 // navigation: {
-  //   nextEl: '.slider-next',
-  //   prevEl: '.slider-prev',
-  // },
-
+  spaceBetween: 10,
+  pagination: {
+    el: '.afisha-pagination'
+  },
+  navigation: {
+    nextEl: '.afisha-navigation-next',
+    prevEl: '.afisha-navigation-prev'
+  },
+  breakpoints: {
+    560: {
+      slidesPerView: 2,
+      adaptiveHeight: true,
+      spaceBetween: 20
+    },
+    991: {
+      slidesPerView: 3,
+      adaptiveHeight: true,
+      spaceBetween: 20
+    },
+    1440: {
+      slidesPerView: 4,
+      adaptiveHeight: true,
+      spaceBetween: 30
+    }
+  }
 });
